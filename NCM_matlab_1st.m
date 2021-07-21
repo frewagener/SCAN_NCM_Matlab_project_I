ae0b3a803c1f1d5a10e137068ddf0745d35c8925
Laura Terlau <laura.terlau@gmail.com>
2021-07-21 18:00:31 +0200

    first matlab script for stimulus initiation

---
+++ NCM_matlab_1st.m
@@ -0,0 +1,104 @@
+% Clear the workspace
+clearvars;
+close all;
+sca;
+
+% Initialize Sounddriver
+InitializePsychSound(1);
+
+% Number of channels and Frequency of the sound
+nrchannels = 2; % 2 because left and right 
+rate = 48000; % rate = samples/second
+
+% repetitions = 1; % default value 
+
+% Length of the beep
+beepLengthSecs = 1;
+
+% Length of the pause between beeps
+beepPauseTime = 1;
+
+% Start immediately (0 = immediately)
+startCue = 0;
+
+% Wait for the device to really start (1 = yes)
+% INFO: See help PsychPortAudio
+waitForDeviceStart = 1;
+
+pahandle = PsychPortAudio('Open', [], 1, 1, rate, nrchannels);
+
+% Make a beep which we will play back to the user
+beep1 = MakeBeep(100, beepLengthSecs, rate);
+beep2 = MakeBeep(150, beepLengthSecs, rate);
+beep3 = MakeBeep(200, beepLengthSecs, rate);
+beep4 = MakeBeep(250, beepLengthSecs, rate);
+beep5 = MakeBeep(300, beepLengthSecs, rate);
+beep6 = MakeBeep(350, beepLengthSecs, rate);
+beep7 = MakeBeep(400, beepLengthSecs, rate);
+beep8 = MakeBeep(450, beepLengthSecs, rate);
+
+
+
+
+% improve portability of your code acorss operating systems 
+KbName('UnifyKeyNames');
+% specify key names of interest in the study
+activeKeys = [KbName('A') KbName('K')]; 
+% set value for maximum time to wait for response (in seconds)
+to_wait = 1.5; 
+
+% restrict keyboard input to the keys we're interested in
+RestrictKeysForKbCheck(activeKeys);
+% suppress echo to the command line for keypresses
+ListenChar(2);
+
+% To do:  get the time stamp at the start of waiting for key input 
+% so we can evaluate timeout and reaction time
+
+for i = 1:3
+    % To DO: randomization
+    seq_1 = PsychPortAudio('FillBuffer', pahandle, [beep1 beep2 beep3; beep1 beep2 beep3]);
+    seq_1;
+    % Wait for the beep to end
+    [actualStartTime, ~, ~, estStopTime] = PsychPortAudio('Stop', pahandle, 1, 1);
+
+    % Compute new start time for follow-up beep, beepPauseTime after end of previous one
+    startCue = estStopTime + beepPauseTime;
+
+    % Start audio playback
+    PsychPortAudio('Start', pahandle, [], startCue, waitForDeviceStart);
+
+    % Wait for stop of playback
+    PsychPortAudio('Stop', pahandle, 1, 1);
+    
+    % tStart can  be the timestamp for the onset of stimuli 
+    tStart = GetSecs;
+    % repeat until a valid key is pressed or we time out
+    time_out = false;
+    % initialise fields for response variable 
+    % that would contain details about the response if given
+    rsp.RT = NaN; rsp.keyCode = []; rsp.keyName = [];
+    while ~time_out
+        % check if a key is pressed
+        % only keys specified in activeKeys are considered valid
+        [ keyIsDown, keyTime, keyCode ] = KbCheck; 
+         if(keyIsDown), break; end
+        if( (keyTime - tStart) > to_wait), time_out = true; end
+    end
+    % store code for key pressed and reaction time
+    if(~time_out)
+        rsp.RT      = keyTime - tStart;
+        rsp.keyCode = keyCode;
+        rsp.keyName = KbName(rsp.keyCode);
+    end
+
+end
+% reset the keyboard input checking for all keys
+RestrictKeysForKbCheck;
+% re-enable echo to the command line for key presses
+% if code crashes before reaching this point 
+% CTRL-C will reenable keyboard input
+ListenChar(1)
+% Close the audio device
+PsychPortAudio('Close', pahandle);
+
