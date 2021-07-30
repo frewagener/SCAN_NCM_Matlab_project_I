6a72fae7215d9da3758438df986aeb91d1d299c7
antoniyaaboyanova <86653715+antoniyaaboyanova@users.noreply.github.com>
2021-07-30 15:55:26 +0200

    Add files via upload

---
+++ Participant_input.m
@@ -0,0 +1,6 @@
+function data = Participant_input()
+disp('Welcome to our experiment')
+data.subjnum = input('Subject Number: ');
+data.age = input('Age: ');
+data.gender = input('Gengder: ', 's');
+end
